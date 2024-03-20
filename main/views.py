224e77cab19652b24cfcce505700fd9f64925c5b
Denis <kopyloveden@gmail.com>
2024-03-21 00:24:36 +0500

    add home page

--- main/views.py
+++ main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    context = {
+        'title': 'home',
+        'content': 'something'
+    }
+    return render(request, 'main/index.html', context)
+    
+
+def about(request):
+    return HttpResponse('About page')
